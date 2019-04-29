f08bbb6993737ce7f6f72710b24649124a13130c
danielferromeral <danielfrgi98@gmail.com>
2019-04-29 19:01:58 +0200

    Add routes mensajes

--- chat/chat.js
+++ chat/chat.js
@@ -9,52 +9,14 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-var Message = mongoose.model('Message',{
-    name : String,
-    message : String
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Message = mongoose.model('Mensaje',{
+    mensaje : String,
+    ID_usuario : ObjectId,
+    ID_oferta : ObjectId
 })
 
 var dbUrl = 'mongodb+srv://admin:admin@mywallapop-crogy.mongodb.net/test?retryWrites=true'
-
-app.get('/messages', (req, res) => {
-    Message.find({},(err, messages)=> {
-        res.send(messages);
-    })
-})
-
-
-app.get('/messages/:user', (req, res) => {
-    var user = req.params.user
-    Message.find({name: user},(err, messages)=> {
-        res.send(messages);
-    })
-})
-
-
-app.post('/messages', async (req, res) => {
-    try{
-        var message = new Message(req.body);
-
-        var savedMessage = await message.save()
-        console.log('saved');
-
-        var censored = await Message.findOne({message:'badword'});
-        if(censored)
-            await Message.remove({_id: censored.id})
-        else
-            io.emit('message', req.body);
-        res.sendStatus(200);
-    }
-    catch (error){
-        res.sendStatus(500);
-        return console.log('error',error);
-    }
-    finally{
-        console.log('Message Posted')
-    }
-
-})
-
 
 
 io.on('connection', () =>{
@@ -68,3 +30,10 @@ mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
 var server = http.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
+
+require("./chat/rmensajes.js")(app,mongoose,io,Message); // (app, param1, param2, etc.)
+
+
+
+
+
