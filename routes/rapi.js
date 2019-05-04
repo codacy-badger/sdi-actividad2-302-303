0a398cb0b86231511de488ae3fd32569596b161f
danielferromeral <47207636+danielferromeral@users.noreply.github.com>
2019-05-04 13:59:53 +0200

    Update rapi.js

--- routes/rapi.js
+++ routes/rapi.js
@@ -1,5 +1,32 @@
 module.exports = function (app, gestorBD) {
 
+    app.post("/api/autentificar/", function(req, res) {
+        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        var criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401); // Unauthorized
+                res.json({
+                    autenticado : false
+                })
+            } else {
+                var token = app.get('jwt').sign(
+                    {usuario: criterio.email, tiempo: Date.now() / 1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado: true,
+                    token: token
+                });
+            }
+        });
+    });
+
     app.get("/api/producto", function(req, res) {
 
         var criterio={vendedor :{$ne: res.usuario}}
@@ -16,87 +43,10 @@ module.exports = function (app, gestorBD) {
         });
     });
 
-    app.post("/api/autentificar/", function(req, res) {
-        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        var criterio = {
-            email : req.body.email,
-            password : seguro
-        }
-
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
-            if (usuarios == null || usuarios.length == 0) {
-                res.status(401); // Unauthorized
-                res.json({
-                    autenticado : false
-                })
-            } else
-            {
-                var token = app.get('jwt').sign(
-                    {usuario: criterio.email, tiempo: Date.now() / 1000},
-                    "secreto");
-                res.status(200);
-                res.json({
-                    autenticado: true,
-                    token: token
-                });
-            }
-        });
-    });
-
-    //Este metodo es para el usuario, y luego debo hacer otro para el dueño
-    //Porque la cosa es que este va a iniciar y enviar mensajes
-    //Pero el otro solo va a enviar mensajes, y antes debe de ver los chats para la oferta
-    //De donde sacara a quien quiere enviar el mensaje
     app.post("/api/mensaje", function(req, res) {
-        var oferta = gestorBD.mongo.ObjectID(req.header['id_oferta']);
+        var IDoferta=req.headers['id_oferta'] || req.body.id_oferta || req.query.id_oferta;
+        var oferta = gestorBD.mongo.ObjectID(IDoferta);
         var criterio = {"_id" : oferta }
-        gestorBD.obtenerMensajes([{"oferta":oferta},{"usuario":res.usuario}],function (conversacion) {
-                if(conversacion==null){
-                    var chat = {
-                        oferta: oferta,
-                        usuario: res.usuario,
-                        conversacion : [
-                            {
-                                texto: req.body.mensaje,
-                                autor: res.usuario,
-                                time: Date.now(),
-                                leido: false
-                            }
-                        ]
-                    }
-                    gestorBD.insertarMensaje(conversacion, function (id) {
-                        if (id == null) {
-                            res.send("Error al insertar mensaje");
-                        }else{
-                            res.send("Mensaje insertado con exito");
-                        }
-                    });
-                }else{
-                    var criterio= {usuario:res.usuario,oferta:oferta}
-                    var conversacion =
-                        {
-                            texto: req.body.mensaje,
-                            autor: res.usuario,
-                            time: Date.now(),
-                            leido: false
-                        }
-                    gestorBD.insertarConversacion(criterio, conversacion, function(bien){
-                        if(!bien){
-                            res.send("Error al insertar conversacion");
-                        }else{
-                            res.send("Conversacion insertada con exito");
-                        }
-                    });
-                }
-        });
-    });
-
-    app.get("/api/mensajes", function(req, res) {
-
-        var IDoferta = gestorBD.mongo.ObjectID(req.headers['id']);
-        var criterio = {_id: IDoferta}
-        var criterioMensajes
         gestorBD.obtenerProductos(criterio, function (producto) {
             if (producto == null) {
                 res.status(500);
@@ -105,10 +55,148 @@ module.exports = function (app, gestorBD) {
                 })
             } else {
                 if (producto[0].vendedor == res.usuario) {
-                    criterioMensajes={oferta:IDoferta}
+                    var to=req.body.to
+                    var criterioRes={"oferta":oferta,"usuario":res.usuario}
+                    gestorBD.obtenerMensajes(criterioRes,function (conversacion) {
+                        if(conversacion==null){
+                            res.send("No existe la conversacion");
+                        }else{
+                            var criterio= {"usuario":to,"oferta":oferta}
+                            var conversacion =
+                                {
+                                    texto: req.body.mensaje,
+                                    autor: res.usuario,
+                                    time: Date.now(),
+                                    leido: false
+                                }
+                            gestorBD.insertarConversacion(criterio, conversacion, function(bien){
+                                if(!bien){
+                                    res.send("Error al insertar conversacion");
+                                }else{
+                                    res.send("Conversacion insertada con exito");
+                                }
+                            });
+                        }
+                    });
                 } else {
-                    criterioMensajes={oferta:IDoferta, from:res.usuario}
+                    var criteroMEN={"oferta":oferta,"usuario":res.usuario}
+                    gestorBD.obtenerMensajes({"oferta":oferta,"usuario":res.usuario},function (conversacion) {
+                        if(conversacion==null) {
+                            var chat = {
+                                oferta: oferta,
+                                usuario: res.usuario,
+                                conversacion: [
+                                    {
+                                        texto: req.body.mensaje,
+                                        autor: res.usuario,
+                                        time: Date.now(),
+                                        leido: false
+                                    }
+                                ]
+                            }
+                            gestorBD.insertarMensaje(chat, function (id) {
+                                if (id == null) {
+                                    res.send("Error al insertar mensaje");
+                                } else {
+                                    res.send("Mensaje insertado con exito");
+                                }
+                            });
+                        }else if(conversacion[0]==null){
+                            var chat = {
+                                oferta: oferta,
+                                usuario: res.usuario,
+                                conversacion : [
+                                    {
+                                        texto: req.body.mensaje,
+                                        autor: res.usuario,
+                                        time: Date.now(),
+                                        leido: false
+                                    }
+                                ]
+                            }
+                            gestorBD.insertarMensaje(chat, function (id) {
+                                if (id == null) {
+                                    res.send("Error al insertar mensaje");
+                                }else{
+                                    res.send("Mensaje insertado con exito");
+                                }
+                            });
+                        }else{
+                            var criterio= {"usuario":res.usuario,"oferta":oferta}
+                            var conversacion =
+                                {
+                                    texto: req.body.mensaje,
+                                    autor: res.usuario,
+                                    time: Date.now(),
+                                    leido: false
+                                }
+                            gestorBD.insertarConversacion(criterio, conversacion, function(bien){
+                                if(!bien){
+                                    res.send("Error al insertar conversacion");
+                                }else{
+                                    res.send("Conversacion insertada con exito");
+                                }
+                            });
+                        }
+                    });
                 }
+            }
+        });
+    });
+
+    app.get("/api/conversacion", function(req, res) {
+
+        var IDoferta=req.headers['id_oferta'] || req.body.id_oferta || req.query.id_oferta;
+        var oferta = gestorBD.mongo.ObjectID(IDoferta);
+        var criterio={"_id":oferta}
+        gestorBD.obtenerProductos(criterio, function (producto) {
+            if (producto == null) {
+                res.status(500);
+                res.json({
+                    error: "se ha producido un error"
+                })
+            } else {
+                if (producto[0].vendedor == res.usuario) {
+                    gestorBD.obtenerMensajes( {oferta:oferta} , function(mensajes) {
+                        if (mensajes == null) {
+                            res.status(500);
+                            res.json({
+                                error: "se ha producido un error"
+                            })
+                        } else {
+                            res.status(200);
+                            var array = []
+                            for (var i = 0; i < mensajes.length; i++) {
+                                array.push(mensajes[i].usuario);
+                            }
+                            res.send(array);
+                        }
+                    });
+                } else {
+                    res.send("Error al ver conversacion");
+                }
+            }
+        });
+    });
+
+    app.get("/api/mensajes", function(req, res) {
+
+        var IDoferta=req.headers['id_oferta'] || req.body.id_oferta || req.query.id_oferta;
+        var oferta = gestorBD.mongo.ObjectID(IDoferta);
+        var criterio={"_id":oferta}
+        gestorBD.obtenerProductos(criterio, function (producto) {
+            if (producto == null) {
+                res.status(500);
+                res.json({
+                    error: "se ha producido un error"
+                })
+            } else {
+                if (producto[0].vendedor == res.usuario) {
+                    criterioMensajes={oferta:oferta}
+                } else {
+                    criterioMensajes = {oferta: oferta, usuario: res.usuario}
+                }
+                console.log(criterioMensajes)
                 gestorBD.obtenerMensajes( criterioMensajes , function(mensajes) {
                     if (mensajes == null) {
                         res.status(500);
@@ -116,8 +204,13 @@ module.exports = function (app, gestorBD) {
                             error : "se ha producido un error"
                         })
                     } else {
+                        console.log(mensajes)
                         res.status(200);
-                        res.send( JSON.stringify(mensajes) );
+                        var array = []
+                        for (var i = 0; i < mensajes[0].conversacion.length; i++) {
+                            array.push(mensajes[0].conversacion[i]);
+                        }
+                        res.send( JSON.stringify(array) );
                     }
                 });
             }
