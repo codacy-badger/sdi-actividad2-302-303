cf8c52e19c9208a34f88f5a23efc62ae0fb4cf0b
yagogr98 <yagogr98@gmail.com>
2019-05-04 11:23:16 +0200

    arreglos cliente

--- app.js
+++ app.js
@@ -22,13 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true }));
 
-// Variables
-/*app.set('port', 8081);
-app.set('db','mongodb://admin:admin@mywallapop-shard-00-00-crogy.mongodb.net:27017,' +
-    'mywallapop-shard-00-01-crogy.mongodb.net:27017,' +
-    'mywallapop-shard-00-02-crogy.mongodb.net:27017/test?ssl=true&replicaSet=myWallapop-shard-0&authSource=admin&retryWrites=true');
-app.set('clave','abcdefg'); app.set('crypto',crypto);
-*/
 
 
 app.use(function(req, res, next) {
